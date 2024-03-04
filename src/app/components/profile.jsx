88d587bfa7ad1c811081c0b484c4ecbf67487cf4
vinicodex <vinicodex@gmail.com>
2024-03-03 22:43:35 -0300

    feat:update img tag to prevent issues

--- src/app/components/profile.jsx
+++ src/app/components/profile.jsx
@@ -1,8 +1,10 @@
+import Image from "next/image";
+
 export default function ProfilePicture() {
     return(
         <div className="flex-col justify-center">
             <div className="flex justify-center">
-                <img class="w-32 h-32 rounded-full" src="img/profile.png" alt="" width="384" height="512"></img>
+                <Image className="w-32 h-32 rounded-full" src="/img/profile.png" alt="" width="384" height="512"/>
             </div>
             <div className="flex justify-center mt-10">
                 <p className="font-bold tracking-wide">@vinicodex</p>
--- src/app/page.js
+++ src/app/page.js
@@ -6,10 +6,10 @@ export default function Home() {
   return(
     <div className="w-full h-full flex-col my-10">
       <ProfilePicture/>
-      <Button img="svg/github.svg" link="https://github.com/vinicodex" alt="github-img" text="Github"/>
-      <Button img="svg/youtube.svg" link="#"   alt="youtube-img" text="Youtube"/>
-      <Button img="svg/linkedin.svg" link="https://www.linkedin.com/in/marcos-vinicius-61619111b/"  alt="linkedin-img" text="Linkedin"/>
-      <Button img="svg/instagram.svg" link="https://www.instagram.com/vinicodex" alt="instagram-img" text="Instagram"/>
+      <Button img="/svg/github.svg" link="https://github.com/vinicodex" alt="github-img" text="Github"/>
+      <Button img="/svg/youtube.svg" link="#"   alt="youtube-img" text="Youtube"/>
+      <Button img="/svg/linkedin.svg" link="https://www.linkedin.com/in/marcos-vinicius-61619111b/"  alt="linkedin-img" text="Linkedin"/>
+      <Button img="/svg/instagram.svg" link="https://www.instagram.com/vinicodex" alt="instagram-img" text="Instagram"/>
       <FooterComponent/>
     </div>
   );
