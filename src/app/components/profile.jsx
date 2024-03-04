26ea19559a578a2848a4e2a42152a26d77a5c27c
vinicodex <vinicodex@gmail.com>
2024-03-03 22:57:44 -0300

    fix: fix margin from components for better spacing

--- src/app/components/profile.jsx
+++ src/app/components/profile.jsx
@@ -4,9 +4,9 @@ export default function ProfilePicture() {
     return(
         <div className="flex-col justify-center">
             <div className="flex justify-center">
-                <Image className="w-32 h-32 rounded-full" src="/img/profile.png" alt="" width="384" height="512"/>
+                <Image className="w-36 h-36 rounded-full" src="/img/profile.png" alt="" width="384" height="512"/>
             </div>
-            <div className="flex justify-center mt-10">
+            <div className="flex justify-center mt-4">
                 <p className="font-bold tracking-wide">@vinicodex</p>
             </div>
         </div>
