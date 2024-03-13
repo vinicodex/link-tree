37c41027f0e3df04d37bd7bac9e1baef17976d14
vinicodex <vinicodex@gmail.com>
2024-03-13 20:09:45 -0300

    fix: update buttons positioning

--- src/app/components/button.jsx
+++ src/app/components/button.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Button(props) {
 
     return(
-        <div className="flex justify-center mt-16">
+        <div className="flex justify-center mt-8">
           <a href={props.link} className="flex justify-center w-full h-full" target="_blank">
           <Image src={props.img} alt={props.alt} className="w-6 h-6 absolute left-16 mt-3 ml-4 animate-pulse" width={24} height={24}/>
             <button className="py-2 px-5 bg-slate-200 font-white rounded-full shadow-md hover:bg-slate-100 hover:text-black focus:outline-none focus:ring focus:ring-slate-800 focus:ring-opacity-75 w-3/4 h-12">
